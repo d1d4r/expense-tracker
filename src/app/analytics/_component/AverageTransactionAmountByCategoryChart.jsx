8e9805f6ba5d7808b37b4ed23f0134a5a1b3971a
didar <deddarnawzad@gmail.com>
2024-05-07 23:15:57 +0300

    delete dummy data and fix log error

--- src/app/analytics/_component/AverageTransactionAmountByCategoryChart.jsx
+++ src/app/analytics/_component/AverageTransactionAmountByCategoryChart.jsx
@@ -11,24 +11,22 @@ import {
   YAxis,
 } from "recharts";
 
-const data = [
-  { name: "Month A", income: 4000, expense: 2400, amt: 2400 },
-  { name: "Month B", income: 1500, expense: 3400, amt: 3400 },
-  { name: "Month C", income: 700, expense: 4400, amt: 4400 },
-  { name: "Month D", income: 5200, expense: 400, amt: 7400 },
-];
+const error = console.error;
+console.error = (...args) => {
+  if (/defaultProps/.test(args[0])) return;
+  error(...args);
+};
 
-export default function AverageTransactionAmountByCategoryChart() {
+export default function AverageTransactionAmountByCategoryChart({ data }) {
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="category" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="income" fill="#8884d8" />
-        <Bar dataKey="expense" fill="#82ca9d" />
+        <Bar dataKey="average_amount" fill="#8884d8" />
       </BarChart>
     </ResponsiveContainer>
   );
--- src/app/analytics/_component/IncomeExpenseOverTimeChart.jsx
+++ src/app/analytics/_component/IncomeExpenseOverTimeChart.jsx
@@ -12,28 +12,25 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  { name: "Month A", income: 4000, expense: 2400, amt: 2400 },
-  { name: "Month B", income: 1500, expense: 3400, amt: 3400 },
-  { name: "Month C", income: 700, expense: 4400, amt: 4400 },
-  { name: "Month D", income: 5200, expense: 400, amt: 7400 },
-];
-export default function IncomeExpenseOverTimeChart() {
+const error = console.error;
+console.error = (...args) => {
+  if (/defaultProps/.test(args[0])) return;
+  error(...args);
+};
+
+export default function IncomeExpenseOverTimeChart({ data }) {
   return (
-    <ResponsiveContainer width='100%' height={350}>
+    <ResponsiveContainer width="100%" height={350}>
       <LineChart
-        // width={730}
-        // height={250}
-        // margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         data={data}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="month" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="expense" stroke="#8884d8" />
-        <Line type="monotone" dataKey="income" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="total_expense" stroke="#8884d8" />
+        <Line type="monotone" dataKey="total_income" stroke="#82ca9d" />
       </LineChart>
     </ResponsiveContainer>
   );
