c7c51a7532a1725d4f45326bf6290d1d36f304fd
didar <deddarnawzad@gmail.com>
2024-05-07 23:21:36 +0300

    fetch all essential data to display in chart

--- src/app/analytics/page.jsx
+++ src/app/analytics/page.jsx
@@ -2,6 +2,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DollarSignIcon } from "lucide-react";
 import dynamic from "next/dynamic";
 import AverageTransactionAmountByCategoryChart from "./_component/AverageTransactionAmountByCategoryChart";
+import { Suspense } from "react";
+import IncomeExpenseByMonthChart from "./_component/IncomeExpenseByMonthChart";
 
 const IncomeExpenseOverTimeChartD = dynamic(
   () => import("./_component/IncomeExpenseOverTimeChart"),
@@ -13,9 +15,83 @@ const CategoryIncomeExpenseChartD = dynamic(
   { ssr: false }
 );
 
-export default function AnalyticsPage() {
+const GetAverageTransactionAmountByCategory = async () => {
+  const result = await prisma.$queryRaw`SELECT
+  c.name AS category,
+    AVG(t.amount) AS average_amount
+    FROM transaction t
+    INNER JOIN category c ON t.categoryId = c.id
+    GROUP BY c.name
+    ORDER BY average_amount DESC;`;
+
+  return JSON.parse(JSON.stringify(result));
+};
+
+const GetIncomeExpenseByMonth = async () => {
+  const result = await prisma.$queryRaw`SELECT
+  YEAR(createdAt) AS year,
+  MONTHNAME(createdAt) AS month,
+  SUM(CASE WHEN type = 'income' THEN amount ELSE 0 END) AS total_income,
+  SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END) AS total_expense
+  FROM transaction
+  GROUP BY YEAR(createdAt), MONTH(createdAt)
+  ORDER BY year, month;`;
+
+  return JSON.parse(JSON.stringify(result));
+};
+
+const GetTotalIncomeExpenseOverTime = async () => {
+  const datas = await prisma.$queryRaw`SELECT
+  YEAR(createdAt) AS year,
+  MONTH(createdAt) AS month,
+  SUM(CASE WHEN type = 'income' THEN amount ELSE 0 END) AS total_income,
+  SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END) AS total_expense
+  FROM transaction
+  GROUP BY YEAR(createdAt), MONTH(createdAt)
+  ORDER BY year, month;`;
+  const result = datas.map((data) => {
+    return {
+      ...data,
+      total_income: +data.total_income,
+      total_expense: +data.total_expense,
+    };
+  });
+  return JSON.parse(JSON.stringify(result));
+};
+
+const getTolatIncome = async () => {
+  const result = await prisma.transaction.aggregate({
+    _sum: { amount: true },
+    where: { type: "INCOME" },
+  });
+  console.log("🚀 ~ getTolatIncome ~ result:", result);
+};
+
+const getTolatExpense = async () => {
+  const result = await prisma.transaction.aggregate({
+    _sum: { amount: true },
+    where: { type: "EXPENSE" },
+  });
+};
+
+const getNetIncome = async () => {
+  const result = await prisma.$queryRaw`SELECT
+  SUM(CASE WHEN type = 'income' THEN amount ELSE 0 END) - 
+  SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END) AS net_income
+  FROM transaction;`;
+  console.log("🚀 ~ getTolatExpense ~ result:", result);
+};
+
+export default async function AnalyticsPage() {
+  const data = await GetAverageTransactionAmountByCategory();
+  const data1 = await GetIncomeExpenseByMonth();
+  const data2 = await GetTotalIncomeExpenseOverTime();
+  const totalIncome = await getTolatIncome();
+  const totalExpense = await getTolatExpense();
+  const netIncom = await getNetIncome();
+
   return (
-    <div className="">
+    <div className="grid w-full grid-cols-1 md:grid-cols-6">
       <div className="grid grid-cols-1 grid-rows-3">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
@@ -54,16 +130,16 @@ export default function AnalyticsPage() {
           </CardContent>
         </Card>
       </div>
-      <div className="grid grid-cols-2 grid-rows-1">
-        <Card>
+      <div className="grid grid-cols-1 col-span-5 grid-rows-1 md:grid-cols-2">
+        <Card className="rounded-none">
           <CardHeader>
             <CardTitle>Income/Expense OverTime</CardTitle>
           </CardHeader>
           <CardContent>
-            <IncomeExpenseOverTimeChartD />
+            <IncomeExpenseOverTimeChartD data={data2} />
           </CardContent>
         </Card>
-        <Card>
+        <Card className="rounded-none">
           <CardHeader>
             <CardTitle>Category Income/Expense</CardTitle>
           </CardHeader>
@@ -72,21 +148,21 @@ export default function AnalyticsPage() {
           </CardContent>
         </Card>
 
-        <Card>
+        <Card className="rounded-none">
           <CardHeader>
             <CardTitle>Average Transaction Amount</CardTitle>
           </CardHeader>
           <CardContent>
-            <AverageTransactionAmountByCategoryChart />
+            <AverageTransactionAmountByCategoryChart data={data} />
           </CardContent>
         </Card>
 
-        <Card>
+        <Card className="rounded-none">
           <CardHeader>
             <CardTitle>Top Spending/Earning Categories</CardTitle>
           </CardHeader>
           <CardContent>
-            <AverageTransactionAmountByCategoryChart />
+            <IncomeExpenseByMonthChart data={data1} />
           </CardContent>
         </Card>
       </div>
