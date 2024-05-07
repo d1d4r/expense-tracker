eba097e64de3d1151135646e7420bbbb61873684
didar <deddarnawzad@gmail.com>
2024-05-07 23:20:43 +0300

    IncomeExpenseByMonthChart component to display income expense by month

---
+++ src/app/analytics/_component/IncomeExpenseByMonthChart.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const error = console.error;
+console.error = (...args) => {
+  if (/defaultProps/.test(args[0])) return;
+  error(...args);
+};
+
+
+export default function IncomeExpenseByMonthChart({ data }) {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="total_income" fill="#6664d8" />
+        <Bar dataKey="total_expense" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
