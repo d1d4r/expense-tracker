5a7ca56ceb5edce433bcb83545907ccb881e6a82
didar <deddarnawzad@gmail.com>
2024-05-09 23:19:46 +0300

    delete getCategories function

--- src/app/transactions/create/page.jsx
+++ src/app/transactions/create/page.jsx
@@ -1,28 +1,5 @@
-import TransactionForm from "@/app/transactions/_component/TransactionForm";
-import prisma from "@/lib/db/prisma";
-import Categories from "../_component/Categories";
-import Form from "@/components/common/Form";
+import Form from "@/app/transactions/_component/Form/Form";
 
-const getCategories = async () => {
-  try {
-    const categories = await prisma.category.findMany();
-    const formatCategories = categories.map((category) => {
-      return {
-        label: category.name,
-        value: category.id,
-      };
-    });
-    return { categories: formatCategories };
-  } catch (error) {
-    return { error: error };
-  }
-};
-
-export default async function CreatePage() {
-  // const { categories, error } = await getCategories();
-
-  // if (!categories) {
-  //   return <div>{JSON.stringify(error)}</div>;
-  // }
+export default function CreatePage() {
   return <Form />;
 }
