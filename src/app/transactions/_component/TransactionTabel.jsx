89ab1ac0e6bc15fa081b943a0d28f7347b8cea37
didar <deddarnawzad@gmail.com>
2024-05-19 17:50:59 +0300

    change component name and page page number to tabel

--- src/app/transactions/_component/TransactionTabel.jsx
+++ src/app/transactions/_component/TransactionTabel.jsx
@@ -2,20 +2,23 @@ import { columns } from "@/app/transactions/_component/columns";
 import { DataTable } from "@/components/Tabel/dataTabel";
 import React from "react";
 import { getTransactions } from "../data/transactionData";
+import { RootTabel } from "./tabel/RootTabel";
 
 
-export default async function TransactionTabel() {
+export default async function TransactionTabel({page}) {
 
-  const { transactions, error } = await getTransactions();
+  const { transactions, error } = await getTransactions(page);
 
   if (!transactions) {
     return <p>{error}</p>;
   }
 
+
   return (
-    <DataTable
+    <RootTabel
       columns={columns}
       data={transactions}
+      page={page}
     />
   );
 }
