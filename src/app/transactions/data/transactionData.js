9ded77b391bf38637b3f673601c644eed19aae7e
didar <deddarnawzad@gmail.com>
2024-06-05 21:42:22 +0300

    add where object to search

--- src/app/transactions/data/transactionData.js
+++ src/app/transactions/data/transactionData.js
@@ -2,9 +2,14 @@ import prisma from "@/lib/db/prisma";
 import { jsonSerialize } from "@/utils/helper";
 import { cache } from "react";
 
-export const getTransactions = cache(async (page = 1) => {
+export const getTransactions = cache(async (page = 1, query = "") => {
   try {
     const data = await prisma.transaction.findMany({
+      where: {
+        description: {
+          contains: query,
+        },
+      },
       include: {
         Category: {
           select: {
